0cb34f0461a31137b06b23f9b16f892b25fe2710
karelle <k.bolon.m@gmail.com>
2024-04-23 17:53:59 +0200

    remove SS

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ setInterval(function () {
 
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss SS [<small>]A[</small>]"
+    "h:mm:ss [<small>]A[</small>]"
   );
 
   //Paris
@@ -17,7 +17,5 @@ setInterval(function () {
   let parisTime = moment().tz("Europe/Paris");
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format(
-    "h:mm:ss SS [<small>]A[</small>]"
-  );
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 }, 1000);
